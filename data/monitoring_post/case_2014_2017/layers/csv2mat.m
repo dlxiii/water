f5e7b96fb969c371d489894fb6b856b80e5c60e3
dlxiii <tokyowangyulong@gmail.com>
2019-09-18 06:11:41 +0900

    water

--- data/monitoring_post/case_2014_2017/layers/csv2mat.m
+++ data/monitoring_post/case_2014_2017/layers/csv2mat.m
@@ -1,7 +1,5 @@
-water_surface = csv_reader('water_surface.csv');
-water_middle = csv_reader('water_middle.csv');
-water_bottom = csv_reader('water_bottom.csv');
+water.sf = csv_reader('water_surface.csv');
+water.md = csv_reader('water_middle.csv');
+water.bt = csv_reader('water_bottom.csv');
 
-save('water_surface.mat','water_surface','-v7.3','-nocompression');
-save('water_middle.mat','water_middle','-v7.3','-nocompression');
-save('water_bottom.mat','water_bottom','-v7.3','-nocompression');
+save('water.mat','water','-v7.3','-nocompression');
