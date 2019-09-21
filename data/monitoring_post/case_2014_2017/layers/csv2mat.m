b2ec6fea96a92241b372473b240c6709a10ec9b5
dlxiii <tokyowangyulong@gmail.com>
2019-09-21 23:10:48 +0900

    generate new layer data

--- data/monitoring_post/case_2014_2017/layers/csv2mat.m
+++ data/monitoring_post/case_2014_2017/layers/csv2mat.m
@@ -1,5 +1,5 @@
-water.sf = csv_reader('water_surface.csv');
-water.md = csv_reader('water_middle.csv');
-water.bt = csv_reader('water_bottom.csv');
+water.sf = csv_reader('water_01.csv');
+water.md = csv_reader('water_06.csv');
+water.bt = csv_reader('water_11.csv');
 
 save('water.mat','water','-v7.3','-nocompression');
